b587f27618078becda40b2524a908621960468c6
Raiane Caroline S Teixeira <raianne.caroline@redeinova.net>
2023-10-04 18:03:29 -0300

    update: AlCriação da classe e atributos, métodos e contrutores

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,3 +1,19 @@
 public class ContaCorrente {
 
+    private float limite;
+    private float saldo;
+    private float valorLimite;
+
+    public ContaCorrente(float limite, float saldo, float valorLimite) {
+        this.limite = limite;
+        this.saldo = saldo;
+        this.valorLimite = valorLimite;
+    }
+
+    public void sacar(float valor) {}
+    public void depositar(float valor) {}
+    public void setValorLimite(float valor) {}
+
+
+
 }
